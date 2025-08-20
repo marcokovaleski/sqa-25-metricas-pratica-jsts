d5405dd98e900e0c90aa8184e78b84163778dd8d
pedro <phavilat@hotmail.com>
2025-08-20 02:32:06 -0300

    style(lint config): atualiza identação e estilo do arquivo

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,10 @@ export default [
       "no-inline-comments": "error",
       "capitalized-comments": ["error", "always"],
       "no-trailing-spaces": "error",
-      "no-magic-numbers": ["error", { ignore: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] }],
+      "no-magic-numbers": [
+        "error",
+        { ignore: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10] },
+      ],
       "no-unused-vars": [
         "error",
         { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
